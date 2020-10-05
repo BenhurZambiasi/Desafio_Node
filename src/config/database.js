df69a0c28cd952ca47e72185a53015ce76c78de7
Benhur <benhur_zambiasi@hotmail.com>
2020-10-05 17:27:31 -0300

    .

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ module.exports = {
   dialect: 'postgres',
   host: 'ec2-52-200-82-50.compute-1.amazonaws.com',
   username: 'tlgkuxdgkmvsqg',
-  password: 'node',
+  password: '95572d92c65c6612c166b6159f5d41334d33af93331e03ddfcb68ed96316ac14',
   port: 5432,
   database: 'd2gj865h3ooui0',
   define: {
