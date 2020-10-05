423946aae77ce2609eeeae7c0e76930f24245a91
Benhur <benhur_zambiasi@hotmail.com>
2020-10-05 13:49:34 -0300

    configurando o banco

--- src/config/database.js
+++ src/config/database.js
@@ -1,13 +1,19 @@
-module.exports ={
+module.exports = {
   dialect: 'postgres',
-  host: 'localhost',
-  username:'postgres',
+  host: 'ec2-52-200-82-50.compute-1.amazonaws.com',
+  username: 'tlgkuxdgkmvsqg',
   password: 'node',
-  port: '5433',
-  database: 'desafio',
-  define:{
+  port: 5432,
+  database: 'd2gj865h3ooui0',
+  define: {
     timestamp: true,
     underscored: true,
     underscoredAll: true
+  },
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
   }
 };
