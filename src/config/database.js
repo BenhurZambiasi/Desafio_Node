22ca38dfe990b943727e6e812f817cd67ae43894
Benhur <benhur_zambiasi@hotmail.com>
2020-10-05 23:43:22 -0300

    configuração original

--- src/config/database.js
+++ src/config/database.js
@@ -1,19 +1,18 @@
 module.exports = {
   dialect: 'postgres',
-  host: 'ec2-52-200-82-50.compute-1.amazonaws.com',
-  username: 'tlgkuxdgkmvsqg',
-  password: '95572d92c65c6612c166b6159f5d41334d33af93331e03ddfcb68ed96316ac14',
-  port: 5432,
-  database: 'd2gj865h3ooui0',
+  host: 'localhost',
+  username: 'postgres',
+  password: 'docker',
+  database: 'desafio',
   define: {
     timestamp: true,
     underscored: true,
     underscoredAll: true
   },
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false
-    }
-  }
+  // dialectOptions: {
+  //   ssl: {
+  //     require: true,
+  //     rejectUnauthorized: false
+  //   }
+  // }
 };
