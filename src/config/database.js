c3e34c93dcdb16b2a92cf7722ebfaab228f6bf40
Benhur <benhur_zambiasi@hotmail.com>
2020-10-06 00:28:34 -0300

    configurando heroku

--- src/config/database.js
+++ src/config/database.js
@@ -1,18 +1,18 @@
 module.exports = {
   dialect: 'postgres',
-  host: 'localhost',
-  username: 'postgres',
-  password: 'docker',
-  database: 'desafio',
+  host: 'ec2-54-156-53-71.compute-1.amazonaws.com',
+  username: 'wsiuqslewoyvqf',
+  password: '74824c536a5e7147bcbff34b52a0806237ce5f1708ab2f110641ea1355aa41a5',
+  database: 'dethsbgmq3c0gt',
   define: {
     timestamp: true,
     underscored: true,
     underscoredAll: true
   },
-  // dialectOptions: {
-  //   ssl: {
-  //     require: true,
-  //     rejectUnauthorized: false
-  //   }
-  // }
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  }
 };
