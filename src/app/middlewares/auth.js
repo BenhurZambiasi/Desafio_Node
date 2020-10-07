280d3e7d44c488fc36ab3c365024306efe36f497
Benhur <benhur_zambiasi@hotmail.com>
2020-10-07 16:31:39 -0300

    .

--- src/app/middlewares/auth.js
+++ src/app/middlewares/auth.js
@@ -7,8 +7,8 @@ import authConfig from '../../config/auth';
 export default async (req, res, next) => {
   const authHeader = req.headers.authorization;
 
-  if (authHeader) {
-    return res.status(401).json({ error: 'Token' })
+  if (!authHeader) {
+    return res.status(401).json({ error: 'Token não encontrado' })
   }
   const [, token] = authHeader.split(' ');
 
